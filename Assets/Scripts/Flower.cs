d9b7e7a1e0784ebdcbd111613ff42d922ca87656
Alberto Longo <alber7longo@gmail.com>
2023-07-22 14:06:44 +0200

    added flower picked animation

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -16,10 +16,14 @@ public class Flower : MonoBehaviour
     [Header("Animation")]
     [SerializeField] private float scaleTweenTime;
     [SerializeField] private float waitBeforeScale;
+    private Sequence pulseSequence;
 
     [Header("Particles")]
     [SerializeField] private ParticleSystem flowerParticles;
     [SerializeField] private ParticleSystem leafParticles;
+
+    [SerializeField] private ParticleSystem pickedParticles;
+    [SerializeField] private ParticleSystem pickedLeafParticles;
 
     private bool isPaired = false;
 
@@ -31,6 +35,9 @@ public class Flower : MonoBehaviour
         stemSprite.GetComponent<RedrawAnimation>().Sprites = flowerVisual.stemSprites;
 
         flowerSprite.transform.localPosition = flowerVisual.flowerPosition;
+
+        pickedParticles.Stop();
+        pickedLeafParticles.Stop();
     }
 
     public Color GetColor()
@@ -41,6 +48,12 @@ public class Flower : MonoBehaviour
     public void SetColor(Color color)
     {
         flowerSprite.color = color;
+
+        var pickedMain = pickedParticles.main;
+        pickedMain.startColor = new ParticleSystem.MinMaxGradient(this.GetColor());
+
+        var flowerModule = flowerParticles.main;
+        flowerModule.startColor = new ParticleSystem.MinMaxGradient(this.GetColor());
     }
 
     public bool IsPaired { get => isPaired; set => isPaired = value; }
@@ -48,14 +61,39 @@ public class Flower : MonoBehaviour
     // this animation makes the flower you already picked up easier to distinguish
     public void StartPickedAnimation()
     {
-        transform.DOPunchScale(Vector3.one / 3, scaleTweenTime, 2, 0.3f).SetEase(Ease.InOutSine);
+        // Setting random leaf sprite
+        var textureSheet = pickedLeafParticles.textureSheetAnimation;
+        textureSheet.SetSprite(0, leafSprites[Random.Range(0, leafSprites.Count)]);
+
+        pickedParticles.Play();
+        pickedLeafParticles.Play();
+
+        transform.DOPunchScale(Vector3.one / 3, scaleTweenTime, 2, 0.3f).SetEase(Ease.InOutSine)
+                .OnComplete(() => StartPulsingAnimation());
+    }
+
+    private void StartPulsingAnimation()
+    {
+        // Crea una secuencia infinita con DOTween
+        pulseSequence = DOTween.Sequence();
+        pulseSequence.Append(transform.DOScale(1.1f, 0.3f));
+        pulseSequence.SetLoops(-1, LoopType.Yoyo);
+        pulseSequence.OnKill(() => transform.localScale = Vector3.one);
+    }
+
+    private void StopPickedAnimation()
+    {
+        if(pulseSequence != null)
+        {
+            pulseSequence.Kill();
+        }
+
+        pickedParticles.Stop();
+        pickedLeafParticles.Stop();
     }
 
     public void StartPairedAnimation()
     {
-        var mainModule = flowerParticles.main;
-        mainModule.startColor = new ParticleSystem.MinMaxGradient(this.GetColor());
-
         // Setting random leaf sprite
         var textureSheet = leafParticles.textureSheetAnimation;
         textureSheet.SetSprite(0, leafSprites[Random.Range(0, leafSprites.Count)]);
@@ -71,6 +109,9 @@ public class Flower : MonoBehaviour
         // Espera a que termine el sistema de partículas
         yield return new WaitForSeconds(waitBeforeScale);
         // Luego ejecuta la animación de escala
+
+        StopPickedAnimation();
+
         transform.DOScale(new Vector3(1.3f, 1.3f, 1.3f), 0.15f).SetEase(Ease.InOutSine)
             .OnComplete(() => transform.DOScale(new Vector3(0, 0, 0), scaleTweenTime).SetEase(Ease.InOutSine));
     }
