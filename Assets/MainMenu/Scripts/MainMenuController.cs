627ba8b4c83362f05359c5ce4ee857a69019fed0
Alberto Longo <alber7longo@gmail.com>
2023-07-23 13:32:20 +0200

    main menu more art and animations

--- Assets/MainMenu/Scripts/MainMenuController.cs
+++ Assets/MainMenu/Scripts/MainMenuController.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public struct Letter
+{
+    public RectTransform rect;
+    public Vector2 finalPosition;
+}
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private List<Letter> letters;
+
     [SerializeField] private RectTransform mainMenu;
     [SerializeField] private RectTransform helpMenu;
 
@@ -19,6 +30,39 @@ public class MainMenuController : MonoBehaviour
         helpMenuStartYPosition = helpMenu.anchoredPosition.y;
 
         helpMenu.anchoredPosition = new Vector2(helpMenu.anchoredPosition.x, helpMenuEndYPosition);
+    }
+
+    private void Start()
+    {
+        StartLettersAnimation();
+    }
+
+    private void StartLettersAnimation()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.AppendInterval(1.0f);
+
+        foreach (Letter letter in letters)
+        {
+            float randomTime = UnityEngine.Random.Range(0.1f, 0.3f);
+            sequence.Append(letter.rect
+                .DOAnchorPos(letter.finalPosition, randomTime).SetEase(Ease.OutBack));
+        }
+
+        sequence.OnComplete(() => StartLetterShakeLoop());
+    }
+
+    private void StartLetterShakeLoop()
+    {
+        float randomWaitTime = UnityEngine.Random.Range(2.0f, 4.0f);
+        int randomIndex = UnityEngine.Random.Range(0, letters.Count);
+
+        letters[randomIndex].rect.DOShakeAnchorPos(0.7f, 10)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() =>
+            {
+                DOVirtual.DelayedCall(randomWaitTime, StartLetterShakeLoop);
+            });
     }
 
     public void Play()
